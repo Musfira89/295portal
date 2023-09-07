b2a7833fc0101c2f94f65520e0276ea251296846
hiraaziz <hiraaziz1998@gmail.com>
2023-09-07 20:09:48 +0500

    Update page.jsx

--- src/app/(customer)/dashboard/page.jsx
+++ src/app/(customer)/dashboard/page.jsx
@@ -6,28 +6,39 @@ import axios from "axios";
 const Dashboard = () => {
   const [camp, setCamp] = useState();
   const { state, dispatch } = useContext(CampaignContext);
-  const [online, setonline] = useState({ online: false, campid: 0 });
+  const [isonline, setonline] = useState({
+    online: false,
+    payout: 0,
+    campid: 0,
+  });
 
   async function getcampaign() {
     const res = await axios.get("/api/campaign");
     if (res.status === 200) {
       dispatch({ type: "GET_CAMPAIGN", payload: res.data.response });
     }
+    console.log("response : ", res);
   }
 
   async function Availability() {
-    const res = await axios.post("/api/availability?userid:1", online);
-    console.log("response : ", res);
+    const res = await axios.put("/api/employees/available?userid=1", isonline);
   }
   useEffect(() => {
     Availability();
-  });
+  }, [isonline]);
   useEffect(() => {
     getcampaign();
   }, []);
   useEffect(() => {
     setCamp(state);
   }, [state]);
+  function handleSubmit(payout, id) {
+    if (isonline.payout !== payout) {
+      setonline({ online: true, payout: payout, campid: id });
+    } else {
+      setonline({ online: !isonline.online, payout: payout, campid: id });
+    }
+  }
 
   return (
     <div className="flex flex-wrap py-10 px-10 gap-6 ">
@@ -49,10 +60,14 @@ const Dashboard = () => {
                 </div>
               ))}
             <button
-              onClick={() => setonline({ online: !online, campid: c.id })}
-              className="text-white "
+              onClick={() => handleSubmit(c.payout, c.id)}
+              className={`text-white ${
+                c.payout !== isonline.payout ? "bg-slate-500" : "bg-green-500"
+              } p-2`}
             >
-              Start Working
+              {c.payout == isonline.payout && isonline.online
+                ? "Stop"
+                : "Start"}
             </button>
           </div>
         ))}
