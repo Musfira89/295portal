4326a5bcb07c474960f59a05e022a5dde632d02a
hiraaziz <hiraaziz1998@gmail.com>
2023-09-08 22:00:31 +0500

    Update CalculateEarnings.jsx

--- src/app/(portal)/employees/[employeeid]/CalculateEarnings.jsx
+++ src/app/(portal)/employees/[employeeid]/CalculateEarnings.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 const CalculateEarnings = ({ id }) => {
   const [callsToday, setCallsToday] = useState(0);
   const [billablesToday, setBillablesToday] = useState(0);
+  const [earnings, setEarnings] = useState({});
 
   const handleCallsChange = (event) => {
     setCallsToday(event.target.value);
@@ -21,19 +22,28 @@ const CalculateEarnings = ({ id }) => {
     };
 
     try {
-      console.log("Data sent:", data);
       const response = await axios.post(
         `/api/employees/employeeid?id=${id}`,
         data
       );
+      setEarnings(response.data.response[0]);
       console.log("Data sent successfully:", response.data);
     } catch (error) {
       console.error("Error sending data:", error);
     }
   };
 
+  const handleDelete = async (id) => {
+    try {
+      const response = await axios.put(`/api/employees/employeeid?id=${id}`);
+      setEarnings(response.data.response[0]);
+    } catch (error) {
+      console.error("Error deleting data:", error);
+    }
+  };
+
   return (
-    <div className="w-2/3 p-10">
+    <div className="w-1/3 p-10">
       <h1 className="text-xl font-bold">Calculate Earnings</h1>
       <div className="flex flex-col gap-2 mt-4">
         {/* Call Sent Today */}
@@ -66,6 +76,32 @@ const CalculateEarnings = ({ id }) => {
           Submit
         </button>
       </div>
+      {earnings && (
+        <div className="mt-4">
+          <h1 className="font-bold text-gray-600">Employee Today Earnings</h1>
+          <div className="flex flex-col gap-2">
+            <div>
+              <h1 className="font-bold text-gray-600">Calls Today</h1>
+              <p>{earnings.calltoday}</p>
+            </div>
+            <div>
+              <h1 className="font-bold text-gray-600">Billables Today</h1>
+              <p>{earnings.billablestoday}</p>
+            </div>
+            <div>
+              <h1 className="font-bold text-gray-600">Earnings Today</h1>
+              <p>{earnings.earningtoday}</p>
+            </div>
+          </div>
+        </div>
+      )}
+      <button
+        className="bg-[#ff914d] rounded-md px-4 py-2 text-white text-sm font-light tracking-wider 
+        hover:bg-[#f28a4a] shadow-lg transition duration-200 ease-in-out w-36"
+        onClick={() => handleDelete(earnings.userid)}
+      >
+        Delete Today Earnings
+      </button>
     </div>
   );
 };
