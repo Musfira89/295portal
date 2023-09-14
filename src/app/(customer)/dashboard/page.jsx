44fbff670be6f7186a89a172926b73a34b2667bb
hiraaziz <hiraaziz1998@gmail.com>
2023-09-14 16:41:35 +0500

    Update page.jsx

--- src/app/(customer)/dashboard/page.jsx
+++ src/app/(customer)/dashboard/page.jsx
@@ -44,26 +44,31 @@ const Dashboard = () => {
     <div className="flex flex-wrap py-10 px-10 gap-6 ">
       {camp &&
         camp.map((c, ind) => (
-          <div className="bg-black" key={ind}>
-            <div className="bg-[#ff914d] p-2 w-full flex justify-center font-semibold">
-              {c.campaign}
+          <div
+            className="shadow-xl bg-gray-200 rounded-lg w-[300px] flex flex-col items-center"
+            key={ind}
+          >
+            <div className="bg-[#ff914d] rounded-t-lg p-2 w-full flex justify-center font-semibold">
+              <h1 className="font-bold text-xl text-white">{c.campaign}</h1>
             </div>
             {Object.entries(c)
               .slice(2)
               .map(([key, value]) => (
                 <div
                   key={key}
-                  className="flex justify-between items-center gap-16 p-4 "
+                  className="flex justify-between items-center gap-16 p-4 w-full text-xs"
                 >
-                  <div className="text-slate-200">{key}</div>
-                  <div className="text-slate-200">{value}</div>
+                  <div className="text-gray-800  font-bold uppercase">
+                    {key}
+                  </div>
+                  <div className="text-gray-800 font-bold">{value}</div>
                 </div>
               ))}
             <button
               onClick={() => handleSubmit(c.payout, c.id)}
-              className={`text-white ${
-                c.payout !== isonline.payout ? "bg-slate-500" : "bg-green-500"
-              } p-2`}
+              className={` bg-[#ff914d] mb-4 rounded-full px-6 py-2 text-white text-sm font-light tracking-wider 
+              hover:bg-[#f28a4a] shadow-lg transition duration-200 ease-in-out
+                p-2`}
             >
               {c.payout == isonline.payout && isonline.online
                 ? "Stop"
