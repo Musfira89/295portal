ca4de4b84872a1e342f69f1d73903b511b33fa62
hiraaziz <hiraaziz1998@gmail.com>
2023-09-14 14:51:04 +0500

    Update route.js

--- src/app/api/employees/employeesbill/route.js
+++ src/app/api/employees/employeesbill/route.js
@@ -1,3 +1,4 @@
+export const dynamic = 'force-static'
 import { NextResponse } from "next/server";
 import * as schema from '../../../../components/utils/drizzle'
 import { drizzle } from 'drizzle-orm/postgres-js';
